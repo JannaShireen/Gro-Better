4597f516f03796b7844f2ae3f216eacf05afc0a4
Janna <jannashireen@gmail.com>
2023-08-01 23:22:53 +0530

    razorpay error

--- lib/provider/home_state.dart
+++ lib/provider/home_state.dart
@@ -10,7 +10,7 @@ class HomeState extends ChangeNotifier {
     const CommunityScreen(),
     //ExpertScreen(),
     const DoctorsList(),
-    const Messages(),
+    Messages(),
     const ProfileScreen(),
   ];
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,9 +10,8 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
-import package_info_plus
 import path_provider_foundation
-import share_plus
+import share_plus_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -21,7 +20,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
